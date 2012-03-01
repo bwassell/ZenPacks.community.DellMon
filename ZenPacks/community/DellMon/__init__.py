9640f7cd57979c09a2ebef615bd622e5fe718cc6
Bob Wassell <bwassell@zenoss.com>
2012-03-01 10:46:07 -0500

    added zlink mod in init for dell support link based on serial #

--- ZenPacks/community/DellMon/__init__.py
+++ ZenPacks/community/DellMon/__init__.py
@@ -17,6 +17,7 @@ class ZenPack(ZenPackBase):
             ]
 
     def install(self, app):
+	
         if hasattr(self.dmd.Reports, 'Device Reports'):
             devReports = self.dmd.Reports['Device Reports']
             rClass = devReports.getReportClass()
@@ -24,6 +25,7 @@ class ZenPack(ZenPackBase):
                 dc = rClass('Dell PowerEdge Reports', None)
                 devReports._setObject('Dell PowerEdge Reports', dc)
         ZenPackBase.install(self, app)
+	self.setupZProperties(app)
 
     def upgrade(self, app):
         if hasattr(self.dmd.Reports, 'Device Reports'):
@@ -40,3 +42,17 @@ class ZenPack(ZenPackBase):
             devReports = self.dmd.Reports['Device Reports']
 	    if hasattr(devReports, 'Dell PowerEdge Reports'):
                 devReports._delObject('Dell PowerEdge Reports')
+    
+    def setupZProperties(self, app):
+	x = self.dmd.Devices.getSubDevicesGen()
+	for d in x:
+	    if d.getHWManufacturerName() == 'Dell' and d.getHWSerialNumber()[-1]  == '1':
+		#dollar = "$"
+        	    #print "%s YES " % d.getId()
+	        dell_link = '<a href= http://support.dell.com/support/topics/global.aspx/support/my_systems_info/details?c=us&l=en&s=gen&ServiceTag=%s target="_">DELL_SUPPORT_LINK</a>' %  d.getHWSerialNumber()
+		orig_zlinks = d.zLinks
+		if orig_zlinks.find('DELL_SUPPORT_LINK') >= 0: return#print "noop return"
+	        if d.hasProperty('zLinks'):
+		    d.zLinks = d.zLinks + ' | ' + dell_link
+		else:
+		    d._setProperty('zLinks', dell_link)
